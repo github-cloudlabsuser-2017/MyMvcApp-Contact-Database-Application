026c477e93225f45994cc369d26a67ae363413c3
github-cloudlabsuser-2017 <github_cloudlabsuser_2017@cloudlabsaiuser.com>
2024-09-27 00:00:06 +0000

    Add user controller implementation

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,18 +12,28 @@ public class UserController : Controller
         public ActionResult Index()
         {
             // Implement the Index method here
+            return View(userlist);
         }
 
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
             // Implement the details method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
         }
 
         // GET: User/Create
         public ActionResult Create()
         {
             //Implement the Create method here
+            // TODO revisit
+            return View();
+            
         }
 
         // POST: User/Create
@@ -31,13 +41,29 @@ public class UserController : Controller
         public ActionResult Create(User user)
         {
             // Implement the Create method (POST) here
+            if (ModelState.IsValid)
+            {
+                userlist.Add(user);
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
         }
 
         // GET: User/Edit/5
         public ActionResult Edit(int id)
         {
             // This method is responsible for displaying the view to edit an existing user with the specified ID.
-            // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.\
+
+            // TODO revisit
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
         }
 
         // POST: User/Edit/5
@@ -49,12 +75,47 @@ public class UserController : Controller
             // If successful, it redirects to the Index action to display the updated list of users.
             // If no user is found with the provided ID, it returns a HttpNotFoundResult.
             // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                // generate code with check if an error occurs during the edit process, it returns the Edit view to display any validation errors.
+                try
+                {
+                    existingUser.Name = user.Name;
+                    existingUser.Email = user.Email;
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, $"An error occurred while updating the user: {ex.Message}");
+                    // TODO revisit
+                    return View("Edit", user);
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+
+
         }
 
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
-            // Implement the Delete method here
+            // This method is responsible for displaying the view to confirm the deletion of a user with the specified ID.
+            // It retrieves the user from the userlist based on the provided ID and passes it to the Delete view.
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
         }
 
         // POST: User/Delete/5
@@ -62,5 +123,15 @@ public class UserController : Controller
         public ActionResult Delete(int id, IFormCollection collection)
         {
             // Implement the Delete method (POST) here
+
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            userlist.Remove(user);
+            return RedirectToAction("Index");
+           
         }
 }
